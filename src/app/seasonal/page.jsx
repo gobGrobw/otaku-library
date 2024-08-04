f864003fc1772526c9a2e644b8ab3646c24acf88
gobGrobw <albertfoeng12@gmail.com>
2024-08-04 08:13:55 +0700

    fixed boundry error

--- src/app/seasonal/page.jsx
+++ src/app/seasonal/page.jsx
@@ -58,55 +58,53 @@ function Seasonal() {
 	}, []);
 
 	return (
-		<Suspense>
-			<main className="padding-base mt-10 mb-14 grow">
-				{/* Current season animes */}
-				<section>
-					<div className="flex flex-col justify-center items-center mb-10 gap-5">
-						<h1 className="font-semibold text-center text-shadow-light text-3xl">
-							{seasonalAnime[0]?.season.slice(0, 1).toUpperCase() +
-								seasonalAnime[0]?.season.slice(1)}{' '}
-							{seasonalAnime[0]?.year} Anime
-						</h1>
+		<main className="padding-base mt-10 mb-14 grow">
+			{/* Current season animes */}
+			<section>
+				<div className="flex flex-col justify-center items-center mb-10 gap-5">
+					<h1 className="font-semibold text-center text-shadow-light text-3xl">
+						{seasonalAnime[0]?.season.slice(0, 1).toUpperCase() +
+							seasonalAnime[0]?.season.slice(1)}{' '}
+						{seasonalAnime[0]?.year} Anime
+					</h1>
 
-						<form
-							onSubmit={onSubmit}
-							className="flex gap-5 justify-center items-center"
+					<form
+						onSubmit={onSubmit}
+						className="flex gap-5 justify-center items-center"
+					>
+						<select
+							name="season"
+							id="season"
+							className="bg-transparent border-b-2 border-gray-600 pl-2 pb-1"
 						>
-							<select
-								name="season"
-								id="season"
-								className="bg-transparent border-b-2 border-gray-600 pl-2 pb-1"
-							>
-								<option value="spring">Spring</option>
-								<option value="summer">Summer</option>
-								<option value="fall">Fall</option>
-								<option value="winter">Winter</option>
-							</select>
+							<option value="spring">Spring</option>
+							<option value="summer">Summer</option>
+							<option value="fall">Fall</option>
+							<option value="winter">Winter</option>
+						</select>
 
-							<input
-								className="bg-transparent border-b-2 border-gray-600 pl-2 pb-1"
-								type="number"
-								name="year"
-								min={1960}
-								max={new Date().getFullYear()}
-								placeholder={new Date().getFullYear()}
-							/>
-							<button
-								type="submit"
-								className="bg-blue-700 text-white p-1 pl-2 pr-2 rounded-lg shadow-md transition-all hover:bg-blue-800"
-							>
-								Search
-							</button>
-						</form>
-					</div>
-					<UlList
-						className={'grid grid-cols-8 justify-center gap-3 gap-y-10'}
-						array={seasonalAnime}
-					/>
-				</section>
-			</main>
-		</Suspense>
+						<input
+							className="bg-transparent border-b-2 border-gray-600 pl-2 pb-1"
+							type="number"
+							name="year"
+							min={1960}
+							max={new Date().getFullYear()}
+							placeholder={new Date().getFullYear()}
+						/>
+						<button
+							type="submit"
+							className="bg-blue-700 text-white p-1 pl-2 pr-2 rounded-lg shadow-md transition-all hover:bg-blue-800"
+						>
+							Search
+						</button>
+					</form>
+				</div>
+				<UlList
+					className={'grid grid-cols-8 justify-center gap-3 gap-y-10'}
+					array={seasonalAnime}
+				/>
+			</section>
+		</main>
 	);
 }
 
